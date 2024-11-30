957a00374e10eb4736b78f40944b8ef423ee6c33
ninabtolo <145863239+ninabtolo@users.noreply.github.com>
2024-11-29 23:33:13 -0300

    Delete atvi directory

--- atvi/src/modelo/produto.ts
+++
@@ -1,9 +0,0 @@
-export default class Produto {
-    public nome: string;
-    public preco: number;
-
-    constructor(nome: string, preco: number) {
-        this.nome = nome;
-        this.preco = preco;
-    }
-}
